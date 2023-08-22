52e23fe4356342e791df40cef6829282f3ed040a
Zaim211 <yousseftitzaim@gmail.com>
2023-08-22 15:07:29 -0700

    monty.h

--- monty.h
+++ monty.h
@@ -33,7 +33,7 @@ typedef struct instruction_s
 } instruction_t;
 
 void nop(stack_t **stack, unsigned int line_number);
-void op_pop(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
 void push(stack_t **stack, char *i, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);

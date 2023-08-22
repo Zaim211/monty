93e6ddd5a61bb15dcb4942f6ba346c8a00f1bcee
Zaim211 <yousseftitzaim@gmail.com>
2023-08-22 13:00:53 -0700

    pint

--- pint.c
+++ pint.c
@@ -17,22 +17,3 @@ void pint(stack_t **stack, unsigned int line_number)
 	}
 	printf("%d\n", (*stack)->n);
 }
-
-/**
- * pall - print stack's elements
- * @line_number: will not be used , we use attribute not void
- * @stack: pointer to the head of the doubly linked list
- * Return: void
-*/
-void pall(stack_t **stack, unsigned int line_number)
-{
-	stack_t *current = *stack;
-
-	if (stack == NULL || *stack == NULL)
-		return;
-	while (current != NULL)
-	{
-		printf("%d\n", current->n);
-		current = current->next;
-	}
-}

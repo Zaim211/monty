bdc31f4fcd60ed2927ed9d144ca71a64c30f4cc0
Zaim211 <yousseftitzaim@gmail.com>
2023-08-22 13:17:40 -0700

    pint

--- pint.c
+++ pint.c
@@ -17,14 +17,3 @@ void pint(stack_t **stack, unsigned int line_number)
 	}
 	printf("%d\n", (*stack)->n);
 }
-/**
- * nop - do nothing
- * @stack: pointer to to pointer to the doubly linked list
- * @line_number: number of lines in the file
- * Return: void
- */
-void op_nop(stack_t **stack, unsigned int line_number)
-{
-	(void) stack;
-	(void) line_number;
-}

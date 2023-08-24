f6c48ceb153bf823fea8ef0b846062a20bb1387d
Zaim211 <yousseftitzaim@gmail.com>
2023-08-24 13:56:48 -0700

    update

--- alloperations.c
+++ alloperations.c
@@ -70,8 +70,8 @@ void pall(stack_t **stack, unsigned int __attribute__((unused)) line_number)
  */
 void nop(stack_t **stack, unsigned int line_number)
 {
-	(void) stack;
 	(void) line_number;
+	(void) stack;
 }
 
 /**

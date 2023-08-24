f4eb6301462cda123038bfd7d7b958215fad7ef4
Zaim211 <yousseftitzaim@gmail.com>
2023-08-24 15:12:51 -0700

    update

--- alloperations.c
+++ alloperations.c
@@ -100,20 +100,14 @@ void pop(stack_t **stack, unsigned int line_number)
 {
 	stack_t *h;
 
-	if (stack == NULL)
+	if (*stack == NULL)
 	{
 		printf("L%u: can't pop an empty stack\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	else if (*stack == NULL)
-	{
-		printf("L%u: can't pop an empty stack\n", line_number);
-		exit(EXIT_FAILURE);
-	}
-	else
-	{
-		h = *stack;
-		*stack = (*stack)->next;
-		free(h);
-	}
+
+	h = *stack;
+	*stack = (*stack)->next;
+	free(h);
+
 }

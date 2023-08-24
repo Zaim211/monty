09de6079d9064910b67dade0d60ccac7ddb05ef3
Zaim211 <yousseftitzaim@gmail.com>
2023-08-24 15:44:00 -0700

    update

--- swap.c
+++ swap.c
@@ -8,14 +8,14 @@
 **/
 void swap(stack_t **stack, unsigned int line_number)
 {
-	int x;
+	int temp;
 
 	if (len(stack) < 2)
 	{
 		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	x = (*stack)->n;
+	temp = (*stack)->n;
 	(*stack)->n = (*stack)->next->n;
-	(*stack)->next->n = x;
+	(*stack)->next->n = temp;
 }

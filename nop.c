8efcbbf6e9be453e0c815f58e6c0b9dbfe423376
Zaim211 <yousseftitzaim@gmail.com>
2023-08-24 18:30:00 -0700

    update

---
+++ nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * nop - do nothing
+ * @stack: pointer to pointer to the doubly linked list
+ * @line_number: number of lines in the file
+ * Return: void
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void) line_number;
+	(void) stack;
+}
---
+++ pint.c
@@ -0,0 +1,17 @@
+#include "monty.h"
+
+/**
+ * pint - print the int at the top of stack
+ * @stack: double pointer th the head
+ * @line_number: number of lines
+ * Return: void
+*/
+void pint(stack_t **stack, unsigned int __attribute__((unused)) line_number)
+{
+	if (stack == NULL || *stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*stack)->n);
+}
